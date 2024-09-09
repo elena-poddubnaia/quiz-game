fabc846e14888e7c08b2deabe0007207e5a446b6
Elena Poddubnaia <elena.poddubnaia@twill.net>
2024-09-09 22:54:02 +0200

    feat: the simpliest version

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref } from 'vue'
 
+const picked = ref('')
 </script>
 
 <template>
+  <main>
+    <h1>Is it first question?</h1>
 
+    <input type="radio" value="yes" id="one" v-model="picked" />
+    <label for="one">yes</label>
+    <br />
+
+    <input type="radio" value="no" id="two" v-model="picked" />
+    <label for="two">no</label>
+    <br />
+
+    {{ picked === '' ? '' : picked === 'yes' ? 'correct' : 'wrong' }}
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
