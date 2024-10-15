bc79eef2ef0c18f1b56c501e0838a5e43ee67cf3
Elena Poddubnaia <elena.poddubnaia@twill.net>
2024-10-15 13:47:31 +0200

    feat: added navigation to home page at button click

--- src/NewGame.vue
+++ src/NewGame.vue
@@ -4,6 +4,16 @@ import { ref } from 'vue'
 
 const store = useStore()
 const questionTitle = ref('')
+
+const handleOnClick = () => {
+  store.commit('addQuestion', {
+    question: questionTitle,
+    options: ['yes', 'no'],
+    answer: 'no',
+    picked: ''
+  })
+  window.location.hash = '#/'
+}
 </script>
 
 <template>
@@ -11,16 +21,5 @@ const questionTitle = ref('')
   <p>Question:</p>
   <input v-model="questionTitle" />
 
-  <button
-    @click="
-      store.commit('addQuestion', {
-        question: questionTitle,
-        options: ['yes', 'no'],
-        answer: 'no',
-        picked: ''
-      })
-    "
-  >
-    Add question
-  </button>
+  <button @click="handleOnClick">Add question</button>
 </template>
